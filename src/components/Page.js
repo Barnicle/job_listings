d48236dbe6264227949af51de65188e9d7b8f5b1
Barnicle <egalimov95@gmail.com>
2020-07-12 14:52:21 +0300

    prep for vercel deploy

---
+++ src/components/Page.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import data from '../data.json';
+import Filters from './filters';
+import List from './list';
+
+export default class App extends Component {
+  state = {
+    data,
+    filters: [],
+  };
+
+  saveFilter = (filter) => {
+    const { filters } = this.state;
+    if (!this.checkFilter(filter)) {
+      filters.splice(filters.length - 1, 0, filter);
+      this.setState({
+        filters,
+      });
+    }
+  };
+
+  filterList = () => {
+    const { data, filters } = this.state;
+    if (filters.length === 0) return data;
+    else return data.filter((el) => el.languages.some((element) => filters.includes(element)));
+  };
+
+  checkFilter = (filter) => this.state.filters.includes(filter); //check if Filter already existed
+
+  deleteFilter = (filter) => {
+    const { filters } = this.state;
+    const result = filters.filter((el) => el != filter);
+    this.setState({
+      filters: result,
+    });
+  };
+
+  clearAllFilters = () => {
+    this.setState({
+      filters: [],
+    });
+  };
+
+  render() {
+    const { data, filters } = this.state;
+    const list = this.filterList(data);
+    return (
+      <div className="app">
+        <header className="header header__size">
+          {filters.length != 0 && (
+            <Filters
+              filters={filters}
+              deleteFilter={this.deleteFilter}
+              clearAllFilters={this.clearAllFilters}
+            />
+          )}
+        </header>
+        <List data={list} handleFilter={this.handleFilter} saveFilter={this.saveFilter} />
+      </div>
+    );
+  }
+}
