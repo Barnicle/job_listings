558fece4d04a933f36a3bea7b0c264d3aed456bc
Barnicle <egalimov95@gmail.com>
2020-07-12 15:07:19 +0300

    fix

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,26 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
+export default class Filters extends Component {
+  handleClearing = () => {
+    this.props.clearAllFilters();
+  };
 
-export default class Filters extends Component{
+  handleRemoveBtn = (e) => {
+    const { target } = e;
+    this.props.deleteFilter(target.value);
+  };
 
-    handleClearing = ()=>{
-        this.props.clearAllFilters();
-    }
-
-    handleRemoveBtn = (e)=> {
-        const {target} = e;
-        this.props.deleteFilter(target.value);
-    }
-
-    render (){
-        const {filters}=this.props;
-        return <div className='filter-list__container'>
-            <div className='filter-list'>
-            {filters.map((el, index)=><div className='filter-list__content' key={index}>
-                <span className='filter__name filter-tag__btn'>{el}</span>
-                <button value={el} className='filter__delete-btn' onClick={this.handleRemoveBtn}>X</button>
-            </div>)}</div>
-            <a className='clear-all' onClick={this.handleClearing}>Clear</a>
+  render() {
+    const { filters } = this.props;
+    return (
+      <div className="filter-list__container">
+        <div className="filter-list">
+          {filters.map((el, index) => (
+            <div className="filter-list__content" key={index}>
+              <span className="filter__name filter-tag__btn">{el}</span>
+              <button value={el} className="filter__delete-btn" onClick={this.handleRemoveBtn}>
+                X
+              </button>
+            </div>
+          ))}
         </div>
-    }
+        <a className="clear-all" onClick={this.handleClearing}>
+          Clear
+        </a>
+      </div>
+    );
+  }
 }
--- src/components/list.js
+++ src/components/list.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ListItem from './list__Item';
+import ListItem from './List__item';
 export default class List extends React.Component {
-
-  handleFilter = (e)=> {
-    const {target} = e;
+  handleFilter = (e) => {
+    const { target } = e;
     this.props.saveFilter(target.innerHTML);
-  }
+  };
   render() {
     const { data } = this.props;
     return (
-      <div className='list'>
+      <div className="list">
         {data.map((el) => (
           <ListItem key={el.id} el={el} handleFilter={this.handleFilter} />
         ))}
